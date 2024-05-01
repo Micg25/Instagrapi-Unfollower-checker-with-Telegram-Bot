f8a0e9d888295069d26baba1bd9eac6d1b1ae1db
Micg25 <gglmhl02l25c351x@studium.unict.it>
2024-05-01 16:30:56 +0200

    first upload

---
+++ Instagrapi_login.py
@@ -0,0 +1,67 @@
+from instagrapi.exceptions import LoginRequired
+import logging
+from instagrapi import Client
+from instagrapi.extractors import extract_user_short
+import json
+import os
+def login_user(USERNAME, PASSWORD, cl, logger):
+    """
+    Attempts to login to Instagram using either the provided session information
+    or the provided username and password.
+    """
+    session_path=""  #put the path of the session.json file here
+    login_via_session = False
+    login_via_pw = False
+
+    if os.path.exists(session_path) and os.path.getsize(session_path) > 0:
+        session = cl.load_settings(session_path) 
+        try:
+            cl.set_settings(session)
+            cl.login(USERNAME, PASSWORD)
+            try:
+                # Check if session is valid
+                cl.get_timeline_feed()
+            except LoginRequired:
+                logger.info("Session is invalid, need to login via username and password")
+                cl.set_settings({})
+                cl.set_uuids(session["uuids"])  #use the same device across logins
+                cl.login(USERNAME, PASSWORD)
+                login_via_pw = True
+            login_via_session = True
+        except Exception as e:
+            logger.info("Couldn't login user using session information: %s" % e)
+            login_via_session = False
+
+    if not login_via_session and not login_via_pw:
+        try:
+            logger.info("Attempting to login via username and password. username: %s" % USERNAME)
+            cl.login(USERNAME, PASSWORD)
+            login_via_pw = True
+            
+        except Exception as e:
+            logger.info("Couldn't login user using username and password: %s" % e)
+            
+    if login_via_pw and os.path.exists(session_path):
+        # Save session settings
+        session = cl.get_settings()
+        with open(session_path, "w") as json_file:
+            json.dump(session, json_file)
+
+    if not login_via_pw and not login_via_session:
+        raise Exception("Couldn't login user with either password or session")
+
+def main(): #this main was created just for debugging
+
+    logger = logging.getLogger()
+    cl = Client()
+    
+    USERNAME = ""
+    PASSWORD = ""
+    
+    
+    login_user(USERNAME, PASSWORD,cl,logger)
+    user_id = cl.user_id_from_username(USERNAME)
+    print(user_id) 
+
+if __name__ == "main":
+    main()
---
+++ Unfollower checker with telebot.py
@@ -0,0 +1,80 @@
+from instagrapi import Client
+import time
+import Instagrapi_login as pl
+import telebot
+
+ACCOUNT_USERNAME=""
+ACCOUNT_PASSWORD=""
+cl = Client()
+logger = pl.logging.getLogger()
+pl.login_user(ACCOUNT_USERNAME, ACCOUNT_PASSWORD,cl,logger)
+
+user_id=cl.user_id_from_username(ACCOUNT_USERNAME)  #Instead of ACCOUNT_USERNAME you can put any public profile username and check their follower/unfollower.
+                                                    #If you want to track your own instagram account's unfollowers I suggest you to log in with a fake account 
+                                                    #then put your own profile to public and run the code but change ACCOUNT_USERNAME to your own username
+key="" #Put the key of your bot, given by BotFather
+bot=telebot.TeleBot(key)
+id=0   #Instead of Zero put the id of the chat you want the bot to work in
+idme=0 #Instead of Zero put your own telegram ID, we will need it later so the bot will know that YOU are writing 
+time.sleep(1)
+
+
+unf=telebot.types.BotCommand("/unfollowers","It will print the list of your unfollowers on instagram")
+bot.set_my_commands([unf])
+infos=bot.get_chat(id)          #It gets the infos of our chat where the bot is working in, 
+                                #we need it in order to get the info of the pinned message, wich is where we store the current follower list
+bot.send_message(id,"STARTING...")
+
+def unfollowers() :
+ if infos.pinned_message is not None:  #check if there is a stored list of our followers
+    users_id1=[]
+    users_id2=[]
+    strunf2=""
+    fw=cl.user_followers(user_id, amount = 0)
+    for user in fw:
+        users_id2.append(user)
+        strunf2=strunf2+user+"\n" #string that will store the newest follower in the pinned message
+
+    
+    users_id1=infos.pinned_message.text.split("\n") #it gets the ids of current follower by the pinned message and it stores them in the users_id1 list
+
+    set1=set(users_id1) 
+    set2=set(users_id2)   
+    unfollowers= set1 - set2
+    strunf="" 
+    
+    i=1
+    if len(unfollowers) >0:
+        bot.send_message(id, "Unfollowers were found...")
+        for x in unfollowers:
+            strunf= strunf + ("Unfollower Number "+ str(i)+" "+cl.username_from_user_id(x)+"\n")
+            time.sleep(1)
+            i+=1 
+        bot.send_message(id, "Unfollowers list: \n" + strunf)
+        messaget=bot.send_message(id,strunf2)
+        bot.pin_chat_message(id,messaget.id)
+    elif set2!=set1: #if the set2 is not equal to set1 it means that there are new followers, so even in this case we update the follower list in the pinned message 
+        bot.send_message(id,"New followers were found, Updating the list...")
+        messaget=bot.send_message(id,strunf2)
+        bot.pin_chat_message(id,messaget.id)
+ else: #if the pinned message is empty we calculate for the first time the list of the followers
+    fw=cl.user_followers(user_id, amount = 0)
+    users_id1=[]
+    for user in fw:
+        users_id1.append(user) 
+    set1=set(users_id1)
+    ris=""
+    for x in set1:
+        ris = ris + (str(x) + "\n")
+    messagep=bot.send_message(id,ris)
+    bot.pin_chat_message(id,messagep.id)
+    bot.send_message(id,"Actual followers have been calculated, restart the bot in order to check if there are any new Unfollowers")
+
+@bot.message_handler(commands=['unfollowers'])
+def handle_test_command(message):
+    sender_id = message.from_user.id
+    if sender_id == idme:
+        unfollowers()
+
+
+bot.polling()
